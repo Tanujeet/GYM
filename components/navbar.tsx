05597771a593177a998d1a511d6dc2f0457b71bc
Tanujeet <singhtanujeet@gmail.com>
2025-10-12 08:25:13 +0530

    Added:Navlinks

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,42 @@
+"use client";
+
 const Navbar = () => {
+  const navlinks = [
+    { label: "Home", href: "home" },
+    { label: "Services", href: "services" },
+    { label: "Team", href: "team" },
+    { label: "Reviews", href: "reviews" },
+  ];
+
+  const handleScroll = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-    <div>Navbar</div>
+    <header>
+      <nav className="border border-b-black rounded-xl p-3 bg-black text-white fixed top-0 left-0 w-full z-50">
+        <div className="flex justify-between items-center max-w-6xl mx-auto">
+          <div>
+            <p className="text-2xl font-bold">Fitness 24</p>
+          </div>
+
+          <div className="flex gap-6">
+            {navlinks.map((link) => (
+              <button
+                key={link.href}
+                onClick={() => handleScroll(link.href)}
+                className="hover:text-gray-400 transition-colors"
+              >
+                {link.label}
+              </button>
+            ))}
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
 
