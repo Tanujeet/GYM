971bbe5909bf3e130ec33037e003d4b6d18c87e0
Tanujeet <singhtanujeet@gmail.com>
2025-10-15 08:16:42 +0530

    Added:CARD Component in second page

--- components/secondpage.tsx
+++ components/secondpage.tsx
@@ -1,8 +1,33 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 const Secondpage = () => {
   return (
     <section>
       <div className="mt-20">
         <h1 className="text-3xl text-center">We offer something to everbody</h1>
+      </div>
+      <div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+            <CardAction>Card Action</CardAction>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
       </div>
     </section>
   );
