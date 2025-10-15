084812b458034fc58bba7af68458ddc5d8add82e
Tanujeet <singhtanujeet@gmail.com>
2025-10-15 08:33:49 +0530

    Added:some minor changes

--- components/secondpage.tsx
+++ components/secondpage.tsx
@@ -8,26 +8,31 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+
+const desc = [
+  { title: "Strength training" },
+  { title: "Strength training" },
+  { title: "Strength training" },
+  { title: "Strength training" },
+];
+
 const Secondpage = () => {
   return (
     <section>
       <div className="mt-20">
         <h1 className="text-3xl text-center">We offer something to everbody</h1>
       </div>
-      <div>
-        <Card>
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-            <CardAction>Card Action</CardAction>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
+      <div className="flex  mt-10 justify-around">
+        {desc.map((item, idx) => (
+          <Card key={idx}>
+            <CardHeader>
+              <CardTitle>{item.title}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+          </Card>
+        ))}
       </div>
     </section>
   );
